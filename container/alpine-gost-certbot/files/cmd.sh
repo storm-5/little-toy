9e32e18114415f1013bc2c7316b46d25a31bf661
storm5 <connect.mr5@gmail.com>
2021-09-01 10:46:41 +0800

    新增alpine-gost-certbot

---
+++ container/alpine-gost-certbot/files/cmd.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -e
+
+CERT_DIR=/etc/letsencrypt
+CERT ${CERT_DIR}/live/${DOMAIN}/fullchain.pem;
+KEY ${CERT_DIR}/live/${DOMAIN}/privkey.pem;
+GOST=/root/files/gost-linux-amd64-2.11.1
+
+# Get certs
+if [ -d ${CERT_DIR} ]; then
+	if [ ! -d ${CERT_DIR}/live/${DOMAIN} ]; then
+		/usr/bin/certbot certonly --standalone --preferred-challenges http \
+			-n -d ${DOMAIN} --email ${EMAIL} --agree-tos --expand
+	else 
+		echo "cert exist. use exist."
+	fi
+else 
+	echo "/etc/letsencrypt not exist"
+	exit 1
+fi
+
+# Kick off cron
+/usr/sbin/crond -f -d 8 &
+
+# start gost
+${GOST} -L "http2://${USER}:${PASS}@0.0.0.0:443?cert=${CERT}&key=${KEY}&probe_resist=code:404&knock=www.google.com" 
---
+++ container/alpine-gost-certbot/start.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+DOMAIN=example.com
+EMAIL=user@gmail.com
+USER=user
+PASS=password
+HTTP_PORT=80
+HTTPS_PORT=443
+LETSENCRYPT_DIR="./files/letsencrypt/"
+
+# run container
+docker run \
+	-d \
+	--name alpine-gost-certbot \
+	-v ${LETSENCRYPT_DIR}:/etc/letsencrypt/ \
+	-p ${HTTP_PORT}:80 \
+	-p ${HTTPS_PORT}:443 \
+	-e DOMAIN=${DOMAIN} \
+	-e EMAIL=${EMAIL} \
+	-e USER=${USER} \
+	-e PASS=${PASS} \
+	alpine-gost-certbot
