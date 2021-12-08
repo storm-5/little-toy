10fcd7f3956266c2e1e815132edb9702f09d9037
storm5 <connect.mr5@gmail.com>
2021-12-08 09:57:46 +0800

    add selinux  relabeling  to mounted volume

--- container/alpine-nginx-certbot/start.sh
+++ container/alpine-nginx-certbot/start.sh
@@ -5,13 +5,17 @@ EMAIL=user@gmail.com
 HTTP_PORT=80
 HTTPS_PORT=443
 LETSENCRYPT_DIR="./files/letsencrypt/"
+NGINX_DIR="./files/nginx/conf.d/"
+NGINX_HTML_DIR="./files/nginx/html/"
 WEBSOCKET_SERVER="localhost:8000"
 
 # run container
 docker run \
 	-d \
 	--name alpine-nginx-certbot \
-	-v ${LETSENCRYPT_DIR}:/etc/letsencrypt/ \
+	-v ${LETSENCRYPT_DIR}:/etc/letsencrypt:z \
+	-v ${NGINX_DIR}:/etc/nginx/conf.d:z \
+    -v ${NGINX_HTML_DIR}:/usr/share/nginx/html:z \
 	-p ${HTTP_PORT}:80 \
 	-p ${HTTPS_PORT}:443 \
 	-e WEBSOCKET_SERVER=${WEBSOCKET_SERVER} \
